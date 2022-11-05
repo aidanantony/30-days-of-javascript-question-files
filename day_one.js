6ab202de397e660bf9d70a60b524c4835c0bb53d
Aidan Antony <aidanantony65@gmail.com>
2022-11-04 23:35:07 -0600

    Third exercise of day 1

--- day_one.js
+++ day_one.js
@@ -1,2 +1,7 @@
 // Exercise 1:  Make a single line comment. Here it is
 // Exercise 2: Make a comment that says "Welcome to 30DaysOfJavaScript" 
+// Exercise 3: Make a multi line comment 
+/* 
+comments can make code readable, easy to reuse and informative
+*/
+
