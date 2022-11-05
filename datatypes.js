088080cc3eca31da306ca265c04f4b434bf9b017
Aidan Antony <aidanantony65@gmail.com>
2022-11-04 23:41:49 -0600

    Fifth exercise of day one

---
+++ datatypes.js
@@ -0,0 +1,10 @@
+//Exercise 5: Checking typeOf
+let string = 'string';
+let boolean = true;
+let noValue = null;
+let nonExistant = undefined;
+
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof noValue);
+console.log(typeof nonExistant);
