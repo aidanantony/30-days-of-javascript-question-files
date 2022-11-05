fdc21c97f02d23f01218cbb63e1dcb0c67ed9f8d
Aidan Antony <aidanantony65@gmail.com>
2022-11-04 23:38:18 -0600

    Fourth exercise of the day

---
+++ variable.js
@@ -0,0 +1,5 @@
+//Fourth exercise of the day: Declare variables and assign string, boolean, undefined and null data types
+let string = 'string';
+let boolean = true;
+let noValue = null;
+let nonExistant = undefined;
