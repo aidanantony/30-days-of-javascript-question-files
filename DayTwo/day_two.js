606261cddb5b2006d82d3ed37ee01a031cbc4091
Aidan Antony <aidanantony65@gmail.com>
2022-11-05 20:45:09 -0600

    Second exercise of day two

--- DayTwo/day_two.js
+++ DayTwo/day_two.js
@@ -1,2 +1,5 @@
 //Exercise 1: Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
 let challenge = '30 Days of Javascript';
+
+//Exercise 2: Print the string on the browser console using console.log()
+console.log(challenge)
