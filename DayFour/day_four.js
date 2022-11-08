8b3695f30355064861d7a0ae16ccada88288f102
Aidan Antony <aidanantony65@gmail.com>
2022-11-07 23:51:53 -0700

    First exercise of day four

---
+++ DayFour/day_four.js
@@ -0,0 +1,15 @@
+/*Exercise 1: Get user input using prompt(“Enter your age:”). 
+If user is 18 or older , give feedback:'You are old enough to drive' 
+but if not 18 give another feedback stating to wait for the number of years he needs to turn 18
+*/
+
+function ageCheck(age) {
+  if(age >= 18) {
+    console.log('You are old enough to drive')
+  }
+  else {
+    console.log(`You must wait ${18 - age} years to drive`)
+  }
+}
+
+ageCheck(14);
