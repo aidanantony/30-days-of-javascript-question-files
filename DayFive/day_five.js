c800f31c2da7677aae1a5af56333984dc8ee48da
Aidan Antony <aidanantony65@gmail.com>
2022-11-08 23:41:07 -0700

    Third exercise of day five

--- DayFive/day_five.js
+++ DayFive/day_five.js
@@ -3,3 +3,6 @@ let emptyArray = [];
 
 //Exercise 2: Declare an array with more than 5 number of elements
 let numArray = [1,2,3,4,5,6,7,8,9]
+
+//Exercise 3: Find the length of your array
+console.log(numArray.length);
