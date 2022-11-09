4888b48dab57720a4fbfd7a154331c4a1d6166b5
Aidan Antony <aidanantony65@gmail.com>
2022-11-09 00:09:21 -0700

    Fourth exercise of day five

--- DayFive/day_five.js
+++ DayFive/day_five.js
@@ -6,3 +6,13 @@ let numArray = [1,2,3,4,5,6,7,8,9]
 
 //Exercise 3: Find the length of your array
 console.log(numArray.length);
+
+//Exercise 4: Get the first item, the middle item and the last item of the array
+let firstValue = numArray[0];
+let middleValue = numArray[Math.round((numArray.length - 1) / 2)];
+let lastValue = numArray[numArray.length - 1];
+
+console.log(firstValue);
+console.log(middleValue);
+console.log(lastValue);
+
