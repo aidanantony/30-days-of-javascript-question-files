489d8ae349133fc18df6c799068a6f4b941bf2e6
Aidan Antony <aidanantony65@gmail.com>
2022-11-17 23:15:08 -0700

    Exercise two of day six

--- DaySix/day_six.js
+++ DaySix/day_six.js
@@ -1,6 +1,10 @@
 //Question 1. For Loop from 0-100 with only evens printed
 for (let i = 0; i<101; i+=2) {
   console.log(i)
-}
+};
 
+//Question 2. For loop from 0-100 with only odd printed
+for (let i=1; i<101; i+=2) {
+  console.log(i)
+};
 
